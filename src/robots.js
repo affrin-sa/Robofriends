bf5e2d3f1a5b29c0066047a25d59cf0e8b1f60e2
affrin-sa <68148181+affrin-sa@users.noreply.github.com>
2020-08-13 22:33:33 -0700

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email:'Sincer@april..biz'
+    },
+    {
+        id:2,
+        name: 'Eravin Howell',
+        username: 'Antonette',
+        email:'shanna@melissa.tv'   
+    },
+    {
+        id:3,
+        name: 'Cleentine Bauch',
+        username: 'samantha',
+        email:'Nathan@yesenia.net'
+    },
+    {
+        id:4,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email:'Sincer@april..biz'
+    },
+    {
+        id:5,
+        name: 'Cleentine Bauch',
+        username: 'samantha',
+        email:'Nathan@yesenia.net'
+    },
+    {
+        id:6,
+        name: 'test test',
+        username: 'Affrin',
+        email:'Karley_Dach@jasner.info'
+    },
+    {
+        id:7,
+        name: 'Kurtis Weissnat',
+        username: 'Sathish',
+        email:'Telly.Hoeger@billy.biz'
+    },
+    {
+        id:8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Saffrin',
+        email:'Sherwood@rosamon.me'
+    },
+    {
+        id:9,
+        name: 'test test',
+        username: 'sumaya sanmathi',
+        email:'sumaya@rosamon.me'
+    },
+    {
+        id:10,
+        name: 'test test',
+        username: 'dall',
+        email:'wood@rosamon.me'
+    }
+]
+
